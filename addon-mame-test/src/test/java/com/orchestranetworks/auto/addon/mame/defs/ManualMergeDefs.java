95b1ea1493cd40d7eded093fc249d369747f543f
Hoang Hue <thihue.hoang@orchestranetworks.com>
2019-08-12 16:06:02 +0700

    Update Mame code to new repo

--- addon-mame-test/src/test/java/com/orchestranetworks/auto/addon/mame/defs/ManualMergeDefs.java
+++ addon-mame-test/src/test/java/com/orchestranetworks/auto/addon/mame/defs/ManualMergeDefs.java
@@ -1,45 +1,71 @@
 package com.orchestranetworks.auto.addon.mame.defs;
 
-import java.util.List;
+import java.util.*;
+import java.util.Map;
 
+import cucumber.api.DataTable;
+import com.orchestranetworks.auto.addon.SessionData;
 import com.orchestranetworks.auto.addon.mame.steps.ManualMergeSteps;
 
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import net.thucydides.core.annotations.Steps;
+import io.vavr.collection.*;
+import java.util.List;
 
 public class ManualMergeDefs {
 	@Steps
 	ManualMergeSteps onManualMergeSteps;
-	
+
 	@Then("^user will see the data as below$")
-    public void user_will_see_the_data_as_below(List<List<String>> tableMerge) throws Throwable {
-		
-		onManualMergeSteps.verify_datatable(tableMerge);
-    }
-	
+	public void user_will_see_the_data_as_below(List<List<String>> tableMerge) throws Throwable {
+		String tblName = "RECORD_VIEW_EXP";
+		SessionData.addDataTable(tblName, tableMerge, false);
+		onManualMergeSteps.verify_datatable(tblName);
+	}
+
 	@And("^table is highlighted and displayed on Preview table as below$")
-    public void table_is_highlighted_and_display_on_preview_table_as_below(List<List<String>> tablePreview) throws Throwable {
-		
+	public void table_is_highlighted_and_display_on_preview_table_as_below(List<List<String>> tablePreview)
+			throws Throwable {
+
 		onManualMergeSteps.verify_table_preview(tablePreview);
-    }
+	}
 
 	@When("^user completes merging process$")
-    public void user_completes_merging_process() throws Throwable {
+	public void user_completes_merging_process() throws Throwable {
 		onManualMergeSteps.click_button_next();
 		onManualMergeSteps.click_button_merge();
-		
-		
-    }
 
+	}
 
-    @Then("^user will see table MappingKey as below$")
-    public void user_will_see_table_mappingkey_as_below() throws Throwable {
-    }
+	@Then("^user will see table RecordMetadata as below$")
+	public void user_will_see_table_recordmetadata_as_below(DataTable recordMetadataExpect) throws Throwable {
+		List<Map<String, String>> list = recordMetadataExpect.asMaps(String.class, String.class);
+		for (int i = 0; i < list.size(); i++) {
+			Map<String, String> row = list.get(i);
+			String recordID = row.get("recordId");
+			String groupID = row.get("groupId");
+			String state = row.get("state");
+			String autoCreated = row.get("autoCreated");
+			String functionalID = row.get("functionalId");
 
-    @Then("^user will see table RecordMetadata as below$")
-    public void user_will_see_table_recordmetadata_as_below() throws Throwable {
-    }
+			if (!recordID.isEmpty()) {
+				onManualMergeSteps.verify_record_value(i, "recordId", recordID);
+			}
+			if (!groupID.isEmpty()) {
+				onManualMergeSteps.verify_group_id(i);
+			}
+			if (!state.isEmpty()) {
+				onManualMergeSteps.verify_record_value(i, "state", state);
+			}
+			if (!autoCreated.isEmpty()) {
+				onManualMergeSteps.verify_record_value(i, "autoCreated", autoCreated);
+			}
+			if (!functionalID.isEmpty()) {
+				onManualMergeSteps.verify_record_value(i, "functionalId", functionalID);
+			}
+		}
 
+	}
 }
