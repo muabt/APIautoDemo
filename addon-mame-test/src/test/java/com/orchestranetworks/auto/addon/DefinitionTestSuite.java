23e2f237be9239a2763e7a52acc6399695e9f8e9
thp <thao.hoang@orchestranetworks.com>
2019-09-27 09:01:15 +0700

    [BEN] Remove tag in MAME project

--- addon-mame-test/src/test/java/com/orchestranetworks/auto/addon/DefinitionTestSuite.java
+++ addon-mame-test/src/test/java/com/orchestranetworks/auto/addon/DefinitionTestSuite.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features = "src/test/resources/features/", tags = "@Survivor_Field_Longest")
+@CucumberOptions(features = "src/test/resources/features/")
 public class DefinitionTestSuite {
 
 }
