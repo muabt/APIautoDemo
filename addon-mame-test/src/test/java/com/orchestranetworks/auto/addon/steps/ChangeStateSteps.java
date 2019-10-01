57bdf88042fd765aef57c1897bcdfb75235daae8
ont <onguyent@tibco.com>
2019-10-01 18:09:17 +0700

    [MAME] Update SC-MTD015, SC-MTD016 and add feature file for Change state

--- addon-mame-test/src/test/java/com/orchestranetworks/auto/addon/steps/ChangeStateSteps.java
+++ addon-mame-test/src/test/java/com/orchestranetworks/auto/addon/steps/ChangeStateSteps.java
@@ -1,4 +1,22 @@
 package com.orchestranetworks.auto.addon.steps;
 
+import com.orchestranetworks.auto.addon.pages.ChangeStatePage;
+import org.junit.Assert;
+
 public class ChangeStateSteps {
+
+    ChangeStatePage onChangeStatepage;
+
+    public void verify_state_descriptions (String stateDes){
+        Assert.assertEquals(stateDes,onChangeStatepage.getChangeStateWidget().getStateDes());
+    }
+
+    public void click_button_launch_service() {
+        onChangeStatepage.getChangeStateWidget().clickBtnLaunchService();
+    }
+
+    public void select_target_state() {
+
+    }
+
 }
